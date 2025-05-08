0e6c60a74d655139bcc50ef1662c0d887b2b0846
charlesgobina <charles.gobina@gmail.com>
2025-05-08 22:33:59 +0300

    chore: optimize for deployment

--- config/admin-config.js
+++ config/admin-config.js
@@ -1,12 +1,13 @@
 // admin config
 import fbAdmin from 'firebase-admin';
+import { initializeApp, applicationDefault } from 'firebase-admin/app';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const serviceAccount = require('../service-account.json');
 
-fbAdmin.initializeApp({
-    credential: fbAdmin.credential.cert(serviceAccount),
-    // storageBucket: "https://console.firebase.google.com/project/devsnap-d2441/storage/devsnap-d2441.firebasestorage.app/files",
+initializeApp({
+    // credential: fbAdmin.credential.cert(serviceAccount),
+    credential: applicationDefault(),
     // databaseURL: process.env.DATABASE_URL, // for realtime database
 });
 
